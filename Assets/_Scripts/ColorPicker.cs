c15f0a7496253f375b52fcf0ae5260c9a9ac4924
Michael Ingargiola <michael.ingargiola@loop.colum.edu>
2018-05-01 17:30:53 -0500

    Color Picker v2
    collors are not buttons changing the color of the sphere through on
    click

--- Assets/_Scripts/ColorPicker.cs
+++ Assets/_Scripts/ColorPicker.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 
 public class ColorPicker : MonoBehaviour
 {
-    [SerializeField] Color selectedColor;
+    Color selectedColor;
     [SerializeField] GameObject playerBall;
 
 	// Use this for initialization
 	void Start ()
     {
         selectedColor = gameObject.GetComponent<Image>().color;
-        DefaultColor();
+        //DefaultColor();
 	}
 
     private void DefaultColor()
@@ -25,19 +25,14 @@ public class ColorPicker : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-        PickColor();
+        //PickColor();
 	}
 
-    void PickColor()
-    {
-        //input: click and tap
-        //select color from options and get the color of that option
-        if (Input.GetMouseButton(0))//left click
-        {
-            //set the player's ball to the selecetd color
-            playerBall.GetComponent<MeshRenderer>().material.color = selectedColor;
+    
 
-        }
+    public void PickColor()
+    {
+            playerBall.GetComponent<MeshRenderer>().material.color = selectedColor;
     }
 
 }
